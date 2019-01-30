20ca7a5b4efbac6e54113e197c7b6f3b980615d8
Hey Dev <hey1dev@gmail.com>
2019-01-30 13:13:28 +0200

    bla bla bla

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+  $(document).ready(function() {
+    $('.offers__slides').slick({
+      dots: false,
+      // autoplay: true,
+      autoplaySpeed: 3000,
+      pauseOnFocus: false,
+      pauseOnHover: false,
+      arrows: true
+    });
+  });
+
+    $(document).ready(function() {
+    $('.hot__slides').slick({
+      dots: false,
+      // autoplay: true,
+      autoplaySpeed: 3000,
+      pauseOnFocus: false,
+      pauseOnHover: false,
+      arrows: true
+    });
+  });
+
